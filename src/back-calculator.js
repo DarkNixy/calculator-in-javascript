060e02aa1ad82a2cc8a19047bae14673c66f74c9
Luiz Araujo <112829903+luizwsaraujo@users.noreply.github.com>
2022-12-01 15:40:34 -0300

    Main

--- src/back-calculator.js
+++ src/back-calculator.js
@@ -1,6 +1,5 @@
-/*Esse bloco de código, Faz uma pergunta para o usuário, aplicação de números digitados
-em variáveis number1 e number2, no final exibindo o resultado finala ao usuário.
-*/
+/* Esse bloco de código, Faz uma pergunta para o usuário, aplicação de números digitados
+em variáveis number1 e number2, no final exibindo o resultado finala ao usuário. */
 var nome = window.prompt("Digite seu Nome:")
 window.alert("Olá " + nome + ", Boas vindas.")
 
@@ -11,10 +10,6 @@ var s = parseInt(number1) + parseInt(number2)
 var div = parseInt(number1) / parseInt(number2)
 var multi = parseInt(number1) * parseInt(number2)
 
-
-
-///////////////////////////////////////////
-window.alert("A soma dos valores é " + s)///////////////
-window.alert("A multiplicação dos valores é " + multi)//
-window.alert("A divisão dos valores é " + div)//////////
-////////////////////////////////////////////////////////
+window.alert("A soma dos valores é " + s)
+window.alert("A multiplicação dos valores é " + multi)
+window.alert("A divisão dos valores é " + div)
